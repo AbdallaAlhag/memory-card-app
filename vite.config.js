bff070cee0dd59e6975004ee71b455f317bc7289
Abdalla Alhag <alhagabdalla@gmail.com>
2024-07-22 20:01:51 -0700

    Work on fixing asset issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this is correct for your deployment
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
